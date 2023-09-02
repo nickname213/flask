6a75e41f57754811c403e662334c90f01030e39c
nickname213 <129471001+nickname213@users.noreply.github.com>
2023-09-01 20:21:14 -0400

    q12

---
+++ streaming.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, Response
+import cv2
+import math
+from ultralytics import YOLO
+
+app = Flask(__name__)
+video_capture = 0  # You can change this to the path of your video if you want to load a video file
+
+cap = cv2.VideoCapture(video_capture)
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
+
+model = YOLO("best.pt")  # Replace with the YOLO model you want to use
+classNames = ["pothole"]  # Replace with the classes you want to detect
+
+def video_detection():
+    while True:
+        success, img = cap.read()
+        if not success:
+            break
+
+        results = model(img, stream=True)
+
+        for r in results:
+            boxes = r.boxes
+            for box in boxes:
+                x1, y1, x2, y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+                conf = math.ceil((box.conf[0] * 100)) / 100
+                cls = int(box.cls[0])
+                class_name = classNames[cls]
+                label = f'{class_name} {conf}'
+                t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+                c2 = x1 + t_size[0], y1 - t_size[1] - 3
+                cv2.rectangle(img, (x1, y1), c2, [255, 0, 255], -1, cv2.LINE_AA)
+                cv2.putText(img, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+
+        out.write(img)
+        ret, buffer = cv2.imencode('.jpg', img)
+        if not ret:
+            continue
+
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/video_feed")
+def video_feed():
+    return Response(video_detection(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+if __name__ == "__main__":
+    app.run(debug=False)
+
+# Release resources
+cap.release()
+out.release()
+cv2.destroyAllWindows()
